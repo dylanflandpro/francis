5a169862d861d7ae7c898c36d1d167e38fd77e19
Dylan F <dylan.fland@gmail.com>
2026-01-23 14:08:30 +0100

    feat: initial commit

---
+++ src/components/form/field-select/index.tsx
@@ -0,0 +1,47 @@
+import { useFormField } from '@/components/form/form-field';
+import { FormFieldContainer } from '@/components/form/form-field-container';
+import { useFormFieldController } from '@/components/form/form-field-controller/context';
+import { FormFieldError } from '@/components/form/form-field-error';
+import { FieldProps } from '@/components/form/types';
+import { Select } from '@/components/ui/select';
+
+export const FieldSelect = (
+  props: FieldProps<
+    {
+      containerProps?: React.ComponentProps<typeof FormFieldContainer>;
+    } & React.ComponentProps<typeof Select>
+  >
+) => {
+  const { containerProps, options, ...rest } = props;
+
+  const ctx = useFormField();
+  const { field, fieldState } = useFormFieldController();
+
+  return (
+    <FormFieldContainer {...containerProps}>
+      <Select
+        invalid={fieldState.invalid}
+        aria-invalid={fieldState.invalid ? true : undefined}
+        aria-describedby={ctx.describedBy(fieldState.invalid)}
+        {...rest}
+        {...field}
+        options={options}
+        value={options.find((option) => option.id === field.value) ?? null}
+        onChange={(e) => {
+          field.onChange(e ? e.id : null);
+          rest.onChange?.(e);
+        }}
+        inputProps={{
+          id: ctx.id,
+          onBlur: (e) => {
+            field.onBlur();
+            rest.inputProps?.onBlur?.(e);
+          },
+          ...rest.inputProps,
+        }}
+      />
+
+      <FormFieldError />
+    </FormFieldContainer>
+  );
+};
